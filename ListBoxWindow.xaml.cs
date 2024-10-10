6bc2cc5f19fb7c98e52e3535f453f36a4ff4d6ec
UesliLaskajs <uesli.laska@gmail.com>
2024-10-10 22:17:23 +0200

    Data Binding

---
+++ ListBoxWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using DataBinding.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public List<Person> People = new List<Person>//From Class Person with Name Age Properties We Create a List
+        {
+            new Person{Name="Uesli",Age=20},//Assigned new Objects
+            new Person{Name="Bruno",Age=13},
+            new Person{Name="Ami",Age=5},
+            new Person{Name="Eno",Age=10},
+        };
+
+        public Window1()
+        {
+            InitializeComponent();
+            ListBoxContent.ItemsSource= People;//We Get The ListBox With Items To beDisplayed with People List We Created
+        }
+
+        private void SelectedClick(object sender ,RoutedEventArgs e)
+        {
+            var listBox=ListBoxContent.SelectedItems;
+
+            foreach(var item in listBox)
+            {
+                var person=(Person)item;
+                MessageBox.Show(person.Name);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DataBinding.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        Person person = new Person()//From Person Class We assign 2 Properties
+        {
+            Name = "Uesli",
+            Age = 20
+        };
+        public MainWindow()
+        {
+
+            this.DataContext = person;//DataContext gets the MainWindow Object inputs
+
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show($"{person.Name} and {person.Age}");
+            Window window = new Window1();//After Click Event is Triggered We Create A new Window
+            window.Show();
+
+
+        }
+    }
+}
